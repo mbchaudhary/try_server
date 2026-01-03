811d36b7200b5f4a8a95a45a6e6dc4e1864daf5a
Mukesh <mbchaudhary9054@gmail.com>
2026-01-03 09:59:57 +0530

    added cors

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
+
+const cors = require('cors');
+app.use(cors());
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
 
 const PORT = process.env.PORT || 3000;
 
